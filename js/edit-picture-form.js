4dea19d6fa9287c3aebe8d97dafba7a46148c7ed
Ml-diana <dbeskorovaynova777@gmail.com>
2022-03-19 13:04:24 +0300

    create code with ptistin

--- js/edit-picture-form.js
+++ js/edit-picture-form.js
@@ -1,22 +1,20 @@
-import {isEscapeKey, clearSelectionField} from './random.js';
+import {isEscapeKey} from './random.js';
 const choosePhoto=document.querySelector('#upload-file');
-//let editRealPhoto=document.querySelector('.img-upload__preview-container');
 const editPhoto=document.querySelector('.img-upload__overlay');
 const modalOpen=document.querySelector('body');
 const choosePhotoClose=document.querySelector('#upload-cancel');
 const pictureForm=document.querySelector('.img-upload__form');
 const inputComment=document.querySelector('.social__footer-text');
-const sendButtonComments=document.querySelector('.social__footer-btn');
+const sendButtonComments=document.querySelector('.img-upload__submit');
 const inputHashtag=document.querySelector('.text__hashtags');
 
 const regular=/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
-const pristine= new Pristine(pictureForm, {
+const pristine = new Pristine(pictureForm, {
   classTo: 'form__field',
   errorTextParent: 'form__field',
   errorTextTag: 'p',
 });
-
 
 const onEditEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -24,14 +22,70 @@ const onEditEscKeydown = (evt) => {
     closeEditPhoto();
   }
 };
+
 const initPhotoForm = () => {
   choosePhoto.addEventListener('change', () => {
     openEditPhoto();
   });
+
   choosePhotoClose.addEventListener('click', () => {
-    closeEditPhoto ();
-    clearSelectionField ();
+    closeEditPhoto();
+    pictureForm.reset();
   });
+
+  pictureForm.addEventListener('submit',(evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    const isValid = pristine.validate();
+    if (isValid){
+      // Тут будет отправка данных на сервер
+    }
+  });
+
+  //незакрытие при фокусе на esc
+  inputHashtag.addEventListener('focus', (evt) => {
+    if (isEscapeKey(evt)){
+      evt.stopPropagation();
+    }
+  });
+
+  inputComment.addEventListener('focus', (evt) => {
+    if (isEscapeKey(evt)){
+      evt.stopPropagation();
+    }
+  });
+
+  inputHashtag.addEventListener('input', (evt) => {
+    sendButtonComments.disabled = !pristine.validate();
+  });
+
+  pristine.addValidator(inputHashtag, (value) => {
+    value = value.trim();
+    if (value.length > 0) {
+      const tags = value.split(/\s+/);
+      for (const tag of tags) {
+        if (!regular.test(tag)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }, 'Тег не соответствует правилам 1');
+
+  //проверка на количество тегов
+  pristine.addValidator(inputHashtag, (value) => {
+    const tags = value.trim().split(/\s+/);
+    return tags.length <= 5;
+  }, 'Тегов должно быть не больше 5');
+
+  //повторы в тегах
+  pristine.addValidator(inputHashtag, (value) => {
+    const tags = value.trim().split(/\s+/);
+    const tagsSet = new Set(tags);
+    return tags.length === tagsSet.size;
+  }, 'Теги не должны повторяться');
+
 };
 
 function openEditPhoto () {
@@ -46,70 +100,5 @@ function closeEditPhoto () {
   document.removeEventListener('keydown', onEditEscKeydown);
 }
 
-inputComment.addEventListener('submit',(evt)=>{
-  evt.preventDefault();
-
-  const isValid=pristine.validate();
-  if (isValid){
-    sendButtonComments.disabled=false;
-  }
-  sendButtonComments.disabled=true;
-});
-
-inputComment.addEventListener('focus', (evt) => {
-  if (isEscapeKey(evt)){
-    evt.stopPropagation();
-  }
-});
-
-pristine.addValidator(inputHashtag, function(value) {
-  const tags = this.value.split(/\s+/);
-  for (let tag=0; tag<=tags.length; tag++){
-    if (!regular.test(tags[tag])){
-      return false;
-    }
-  }
-  return true;
-}, 'Тег не соответствует правилам', 1, false);
-
-pristine.addValidator(inputHashtag, function(value) {
-  const tags = this.value.split(/\s+/);
-  for (let tag of tags){
-    if (!regular.test(tag)){
-      return false;
-    }
-  }
-  return true;
-}, 'Тег не соответствует правилам', 1, false);
-
-pristine.addValidator(inputHashtag, function(value) {
-  const tags = this.value.split(/\s+/);
-  for (let tag=0; tag<=tags.length; tag++){
-    const regTeg=regular.test(tags[tag]);
-    if (regTeg===false){
-      return false;
-    }
-  }
-}, 'Тег не соответствует правилам', 1, false);
-//проверка на количество тегов
-pristine.addValidator(inputHashtag, function(value) {
-  const tags = this.value.split(/\s+/);
-  return tags.length <= 5;
-}, 'Тегов должно быть не больше 5', 1, false);
-
-//повторы в тегах
-pristine.addValidator(inputHashtag, function(value) {
-  const tags = this.value.split(/\s+/);
-  const tagsSet = new Set(tags);
-  return tags.length === tagsSet.size;
-}, 'Теги не должны повторяться', 2, false);
-
-//незакрытие при фокусе на esc
-inputHashtag.addEventListener('focus', (evt) => {
-  if (isEscapeKey(evt)){
-    evt.stopPropagation();
-  }
-});
 
 export{initPhotoForm};
-
