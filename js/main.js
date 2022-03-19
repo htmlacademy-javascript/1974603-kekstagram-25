ecff9b781dbec1612fa5538c8c5f0c38c0ba2712
Ml-diana <dbeskorovaynova777@gmail.com>
2022-03-19 13:36:32 +0300

    create

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {createPhotoObjects} from './photo.js';
 import {renderPictures} from './picture.js';
-import {initPhotoForm} from './edit-picture-form';
+import {initPhotoForm} from './edit-picture-form.js';
 const pictures = createPhotoObjects();
 renderPictures(pictures);
 initPhotoForm();
