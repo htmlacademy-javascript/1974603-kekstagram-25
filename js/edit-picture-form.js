563cd8a40d2afece006f35d57f71805d69bb5c2c
Ml-diana <dbeskorovaynova777@gmail.com>
2022-03-19 16:02:05 +0300

    change comment

--- js/edit-picture-form.js
+++ js/edit-picture-form.js
@@ -7,6 +7,7 @@ const pictureForm=document.querySelector('.img-upload__form');
 const inputComment=document.querySelector('.social__footer-text');
 const sendButtonComments=document.querySelector('.img-upload__submit');
 const inputHashtag=document.querySelector('.text__hashtags');
+const commentText=document.querySelector('.text__description');
 
 const regular=/^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
@@ -56,7 +57,11 @@ const initPhotoForm = () => {
     }
   });
 
-  inputHashtag.addEventListener('input', (evt) => {
+  inputHashtag.addEventListener('input', () => {
+    sendButtonComments.disabled = !pristine.validate();
+  });
+
+  commentText.addEventListener('input', () => {
     sendButtonComments.disabled = !pristine.validate();
   });
 
@@ -71,7 +76,7 @@ const initPhotoForm = () => {
       }
     }
     return true;
-  }, 'Тег не соответствует правилам 1');
+  }, 'Тег не соответствует правилам');
 
   //проверка на количество тегов
   pristine.addValidator(inputHashtag, (value) => {
@@ -86,7 +91,15 @@ const initPhotoForm = () => {
     return tags.length === tagsSet.size;
   }, 'Теги не должны повторяться');
 
+  pristine.addValidator(commentText, () => {
+    if (commentText.value.length>=140){
+      return false;
+    }
+    return true;
+  }, 'Комментарий должен быть меньше 140 символов');
 };
+
+
 
 function openEditPhoto () {
   editPhoto.classList.remove('hidden');
