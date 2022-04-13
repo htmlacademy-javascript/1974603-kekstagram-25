920c0f0d3a5c36cf88a16585641013849c21aadd
Ml-diana <dbeskorovaynova777@gmail.com>
2022-04-13 11:33:28 +0300

    исправлен main

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ import {initScale, initFilters} from './effects-scale.js';
 import {setEditFormSubmit} from './edit-picture-form.js';
 import {closeEditPhoto} from './edit-picture-form.js';
 import{getData} from './server.js';
-import {showAlert,debounce,randomButtonStyle,deleteStyleRandom,deleteStyleDefault,discussButtonStyle,deleteStyleDiscuss,defaultButtonStyle} from './random.js';
+import {showAlert,debounce} from './random.js';
 const defaultButton=document.querySelector('#filter-default');
-//const defaultButton=document.querySelector('img-filters__button  img-filters__button--active');
 const RERENDER_DELAY = 500;
 const imgFilters=document.querySelector('.img-filters');
 const RANDOM_PICTURE=10;
@@ -20,27 +19,33 @@ getData(
     renderPictures(pictures);
     imgFilters.classList.remove('img-filters--inactive');
     defaultButton.addEventListener('click', debounce(()=>{
-      defaultButtonStyle();
-      deleteStyleDiscuss();
-      deleteStyleRandom();
+      if (randomButton.classList.contains('img-filters__button--active')|| discussedButton.classList.contains('img-filters__button--active')){
+        randomButton.classList.remove('img-filters__button--active');
+        discussedButton.classList.remove('img-filters__button--active');
+      }
+      defaultButton.classList.add('img-filters__button--active');
       cleanPictureList();
       renderPictures(pictures);
     },RERENDER_DELAY));
     randomButton.addEventListener('click', debounce(()=>{
-      deleteStyleDefault();
-      deleteStyleDiscuss();
-      randomButtonStyle();
+      if (defaultButton.classList.contains('img-filters__button--active')|| discussedButton.classList.contains('img-filters__button--active')){
+        defaultButton.classList.remove('img-filters__button--active');
+        discussedButton.classList.remove('img-filters__button--active');
+      }
+      randomButton.classList.add('img-filters__button--active');
       cleanPictureList();
       const randomTenPictures=pictures.map((item) => [Math.random(), item]).sort().map((element) => element[1]).slice(0, RANDOM_PICTURE);
       renderPictures(randomTenPictures);
     }, RERENDER_DELAY));
     discussedButton.addEventListener('click',debounce(()=>{
-      discussButtonStyle();
-      deleteStyleRandom();
-      deleteStyleDefault();
+      if (randomButton.classList.contains('img-filters__button--active')|| defaultButton.classList.contains('img-filters__button--active')){
+        randomButton.classList.remove('img-filters__button--active');
+        defaultButton.classList.remove('img-filters__button--active');
+      }
+      discussedButton.classList.add('img-filters__button--active');
       cleanPictureList();
       const comparePictures = (pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length;
-      const mostCommented = pictures.sort(comparePictures).slice(10);
+      const mostCommented = pictures.sort(comparePictures).slice(0,10);
       renderPictures(mostCommented);
     }, RERENDER_DELAY));
   },
